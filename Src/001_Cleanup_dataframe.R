f998266a05a88c06405004994c296024bc43e81b
Envimaster@combat <Schoenb8@staff.uni-marburg.de>
2021-10-14 01:32:23 +0200

    add data new cleaning
    Add hannas data
    New cleaning for DF, adding new column

--- Src/001_Cleanup_dataframe.R
+++ Src/001_Cleanup_dataframe.R
@@ -38,11 +38,67 @@ dfc<-dfc[-231,]
 dfc$PAM <-as.numeric(dfc$PAM)
 dfc$CTR <-as.numeric(dfc$CTR)
 
-# add ID and reorder Colums
+# add ID
 dfc$ID <- 1:nrow(dfc)
-dfc <- dfc[, c(12, 1:11)]
+head(dfc)
+
+# add class for dialect (alternavtive code styling)
+#dfc$diaClass_hanna <- ifelse(grepl("^Nordniederdeutsch$|^Nordniederdeutsch-Ostfälisch$|^Mecklenburgisch-Vorpommersch$|^Mittelpommersch$|^Brandenburgisch-Südmärkisch$|^Brandenburgisch$", dfc$dilect), "nördliches Niederdeutsch",
+#ifelse(grepl("^Westfälisch$|^Ostfälisch$|^Ostfälisch-Brandenburgisch$", dfc$dilect), "südliches Niederdeutsch",
+#ifelse(grepl("^Ripuarisch$|^Ripuarisch-Niederfränkisch$|^Moselfränkisch$|^Moselfränkisch-Ripuarisch$|^Niederfränkisch$", dfc$dilect), "Westdeutsch",
+#ifelse(grepl("^Rheinfränkisch$|^Rheinfränkisch-Moselfränkisch$|^Rheinfränkisch-Moselfränkisch-Zentralhessisch$|^Rheinfränkisch-Ostfränkisch-Schwäbisch$|^Rheinfränkisch-Zentralhessisch-Moselfränkisch$|^Nordhessisch$|^Zentralhessisch$|^Osthessisch$", dfc$dilect), "westliches Mitteldeutsch",
+#ifelse(grepl("^Thüringisch$|^Thüringisch-Obersächsisch$|^Obersächsisch$|^Nordobersächsisch$", dfc$dilect), "östliches Mitteldeutsch",
+#ifelse(grepl("^Ostfränkisch$|^Nordbairisch$|^Nordbairisch-Mittelbairisch$|^Mittelbairisch$|^Südbairisch-Schwäbisch$", dfc$dilect), "Ostoberdeutsch",
+#ifelse(grepl("^Schwäbisch-Mittelbairisch$|^Schwäbisch$|^Hochalemannisch$|^Hochalemannisch/Niederalemannisch$|^Mittelalemannisch$|^Niederalemannisch$", dfc$dilect), "Westoberdeutsch",
+#       0)))))))
+
+dfc$diaClass_hanna <- ifelse(grepl(
+"^Nordniederdeutsch$|^Nordniederdeutsch-Ostfälisch$|^Mecklenburgisch-Vorpommersch$|^Mittelpommersch$|^Brandenburgisch-Südmärkisch$|^Brandenburgisch$"
+, dfc$dilect), "nördliches Niederdeutsch",
+ifelse(grepl(
+"^Westfälisch$|^Ostfälisch$|^Ostfälisch-Brandenburgisch$"
+, dfc$dilect), "südliches Niederdeutsch",
+ifelse(grepl(
+"^Ripuarisch$|^Ripuarisch-Niederfränkisch$|^Moselfränkisch$|^Moselfränkisch-Ripuarisch$|^Niederfränkisch$"
+, dfc$dilect), "Westdeutsch",
+ifelse(grepl(
+"^Rheinfränkisch$|^Rheinfränkisch-Moselfränkisch$|^Rheinfränkisch-Moselfränkisch-Zentralhessisch$|^Rheinfränkisch-Ostfränkisch-Schwäbisch$|^Rheinfränkisch-Zentralhessisch-Moselfränkisch$|^Nordhessisch$|^Zentralhessisch$|^Osthessisch$", dfc$dilect), "westliches Mitteldeutsch",
+ifelse(grepl(
+"^Thüringisch$|^Thüringisch-Obersächsisch$|^Obersächsisch$|^Nordobersächsisch$"
+, dfc$dilect), "östliches Mitteldeutsch",
+ifelse(grepl(
+"^Ostfränkisch$|^Nordbairisch$|^Nordbairisch-Mittelbairisch$|^Mittelbairisch$|^Südbairisch-Schwäbisch$"
+, dfc$dilect), "Ostoberdeutsch",
+ifelse(grepl(
+"^Schwäbisch-Mittelbairisch$|^Schwäbisch$|^Hochalemannisch$|^Hochalemannisch/Niederalemannisch$|^Mittelalemannisch$|^Niederalemannisch$"
+, dfc$dilect), "Westoberdeutsch",
+0)))))))
+
+
+# check if there is any "0", NOTE: pattern syntax need to have all expressions in a row without " " or enter
+unique(dfc$diaClass_hanna)
 
 head(dfc)
-write.xlsx(dfc,file.path(wdd,"full_clean.xlsx"))
-write.csv(dfc,file.path(wdd,"full_clean.csv"))
+
+# rename columns
+colnames(dfc)
+colnames(dfc)[colnames(dfc) == 'dilect'] <- 'dialect' # spell correction :D
+
+# reorder columns
+dfc <- dfc[, c(12, 1,13,2:11),]
+head(dfc)
+write.xlsx(dfc,file.path(wdd,"full_clean.xlsx"),overwrite = T)
+write.csv(dfc,file.path(wdd,"full_clean.csv"))# automatic overwrites
 ###############################################################################
+
+# check correlation betwenn PAM add and PAM
+cor(dfc$PAM,dfc$PAM_add)
+
+unique(dfc$PAM_add)
+
+# get numeric
+test <- dfc
+test$PAM_add <-as.numeric(as.factor(dfc$PAM_add))
+test <-na.omit(test)
+cor(test$PAM,test$PAM_add)
+# ok NO, realy NO correlation
